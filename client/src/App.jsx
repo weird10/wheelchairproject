ece7f63b28dfa9960da2013c44bc6e58de9009d5
weird10 <ryandavidtan10@gmail.com>
2022-11-08 21:03:39 +0700

    wheelchair project initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,6 +28,8 @@ function App() {
       <BrowserRouter>
       <NavBar user={user} setUser={setUser}/>
       <Routes>
+        
+        <Route path = "/"  element={<Login setUser={setUser}/>}></Route>
         <Route path = "/login"  element={<Login setUser={setUser}/>}></Route>
 
         <Route path = "/addWheelchair"   element={<WheelchairForm user={user}/>}></Route>
